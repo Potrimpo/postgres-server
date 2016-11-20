6b7d0e9aba9539246c2ab528434bd322934ad1bd
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-11-21 12:49:26 +1300

    added config files, hopefully workable

--- postgres-setup.sh
+++ postgres-setup.sh
@@ -2,16 +2,9 @@
 sudo apt-get update
 sudo apt-get install postgresql postgresql-contrib
 sudo -u postgres createdb menubot
-sudo nano /etc/postgresql/{version}/main/postgresql.conf
-
-# replace `listen_addressses = 'localhost'` with `listen_addresses = '*'`
-
-sudo vim /etc/postgresql/{version}/main/pg_hba.conf
-
-### add lines
-  # TYPE    DATABASE    USER            ADDRESS                           METHOD
-    host    all         postgres      {postgres instance local ip}/32     md5
-
+# copying config files into place
+sudo cp ./postgresql.conf /etc/postgresql/{version}/main/postgresql.conf
+sudo cp ./pg_hba.conf /etc/postgresql/{version}/main/pg_hba.conf
 
 sudo service postgresql restart
 
